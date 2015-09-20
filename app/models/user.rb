d1a22fdd19cf98427a47f82b420cfd7c763dd140
Phillipp Schoppmann <schoppmp@cms.hu-berlin.de>
2015-09-20 01:09:05 +0100

    fix user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ActiveRecord::Base
 	end
 
 	validates_presence_of :first_name, :last_name, :user_name, :email, :password_digest	
-	validates_uniqueness_of :name, :email, case_sensitive: false 
+	validates_uniqueness_of :user_name, :email, case_sensitive: false 
 	validates_format_of :email, with: /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
 end
