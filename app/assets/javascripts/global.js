b2a494774816b17c864b8e5295040ba3eb8e2de2
Giacomo Mineo <giacomo.mineo91@gmail.com>
2015-10-06 20:08:54 +0200

    group filter for entries

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,6 +5,19 @@ function scrollToAnchor(id_selector){
   $('html,body').animate({ scrollTop: id_selector.offset().top - 52 }, 300);
 }
 
+function filter_by_group(group_element) {
+	group_id = group_element.attr('id');
+	if(!group_element.hasClass('group-nav-current')) {
+		$('.group-nav-current').removeClass('group-nav-current');
+		group_element.addClass('group-nav-current');
+		$('.entry').show();
+		$('.entry:not(.' + group_id + ')').hide();
+	} else {
+		group_element.removeClass('group-nav-current');
+		$('.entry').show();
+	}
+}
+
 // Doc ready
 $(function() {
 	
@@ -45,11 +58,8 @@ $(document).on('page:change', function(event) {
 	$('#signup-btn').removeClass('clicked');
 	//enable tooltips
 	$('[data-toggle="tooltip"]').tooltip();
-	//bind scrolling anchors
-	$('.group-nav-item a').click(function(e) {
-	  e.preventDefault();
-	  if(!($(this).parent().hasClass('group-nav-current'))) {
-	    scrollToAnchor($($(this).attr('href')));
-	  }
+	//filter by group
+	$('.group-nav-item').click(function(event) {
+		filter_by_group($(this));
 	});
 });
