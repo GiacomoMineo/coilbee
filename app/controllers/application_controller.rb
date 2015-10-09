5825d7982d4ca2d801499cd008208b5caa6cbdc6
Veronica Moser <wicinovsky@googlemail.com>
2015-10-09 12:23:14 +0200

    Add user_name to signup and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
   before_filter :set_categories_for_menu
+  before_filter :configure_devise_permitted_parameters, if: :devise_controller?
   helper_method :current_user 
 
   private
@@ -27,6 +29,24 @@ class ApplicationController < ActionController::Base
   def require_admin
   	redirect_to '/' unless current_user.admin? 
   end
+
+  def configure_devise_permitted_parameters
+    registration_params = [:user_name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
+  end
+
+
+
+
 
   def after_sign_in_path_for(resource)
     libraries_path
