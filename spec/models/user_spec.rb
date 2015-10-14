e6a6b0f4f771190420cbd517074355aed809f8dd
Veronica Moser <wicinovsky@googlemail.com>
2015-10-14 11:58:21 +0200

    Improve tests on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	it "is valid with a username, email and password" do
 		user = User.new(
-			#user_name: 'Aaron',
+			user_name: 'Aaron',
 			email: 'tester@example.com',
 			password: 'password')
 		expect(user).to be_valid
@@ -11,8 +11,8 @@ RSpec.describe User, type: :model do
 
 	it "is invalid without a username" do
 		user = User.new(user_name: nil)
-		#user.valid?
-		#expect(user.errors[:user_name]).to include("can't be blank")
+		user.valid?
+		expect(user.errors[:user_name]).to include("can't be blank")
 	end
 	it "is invalid without an email address"
 	it "is invalid with a duplicate username"
