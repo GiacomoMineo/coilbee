5cc72fc289e8c46d412548b6d6da092fe75fc26d
Veronica Moser <wicinovsky@googlemail.com>
2015-10-14 12:25:28 +0200

    Extend user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,46 @@ RSpec.describe User, type: :model do
 		user.valid?
 		expect(user.errors[:user_name]).to include("can't be blank")
 	end
-	it "is invalid without an email address"
-	it "is invalid with a duplicate username"
-	it "is invalid with a duplicate email address"
+
+	it "is invalid without an email address" do
+		user = User.new(email: nil)
+		user.valid?
+		expect(user.errors[:email]).to include("can't be blank")
+	end
+
+	it "is invalid without a password address" do
+		user = User.new(password: nil)
+		user.valid?
+		expect(user.errors[:password]).to include("can't be blank")
+	end
+
+	it "is invalid with a duplicate username" do
+		user = User.create(
+			user_name: 'Aaron',
+			email: 'aaron@example.com',
+			password: 'password')
+		user = User.create(
+			user_name: 'Aaron',
+			email: 'tester@example.com',
+			password: 'password')
+		user.valid?
+		expect(user.errors[:user_name]).to include("has already been taken")
+	end
+
+	it "is invalid with a duplicate email address" do
+		user = User.create(
+			user_name: 'Aaron',
+			email: 'tester@example.com',
+			password: 'password')
+		user = User.create(
+			user_name: 'Paul',
+			email: 'tester@example.com',
+			password: 'password')
+		user.valid?
+		expect(user.errors[:email]).to include("has already been taken")
+	end
+
+
 	it "returns a contact's username as a string"
 	it "returns a contact's email as a string"
 end
