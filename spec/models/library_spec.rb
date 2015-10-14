15f21496df525309931b3a9cba637d3832045711
Veronica Moser <wicinovsky@googlemail.com>
2015-10-14 15:25:53 +0200

    Add model test for library

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Library, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "is valid with a topic, description" do
+		library = Library.new(
+			topic: 'Library-Title',
+			description: 'Library-Description'
+			)
+		expect(library).to be_valid
+	end
+
+	it "is valid without a description" do
+		library = Library.new(
+			topic: "Library-title", 
+			description: nil
+			)
+		expect(library).to be_valid
+	end
+	
+	it "is invalid without a topic" do
+		library = Library.new(topic: nil)
+		library.valid?
+		expect(library.errors[:topic]).to include("can't be blank")
+	end
+
 end
