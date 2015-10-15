1a88759dcf2a216d5b10368f8630920440b4fbde
Veronica Moser <wicinovsky@googlemail.com>
2015-10-15 13:47:59 +0200

    Use library factory in model test

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -3,23 +3,17 @@ require 'rails_helper'
 RSpec.describe Library, type: :model do
 
 	it "is valid with a topic, description" do
-		library = Library.new(
-			topic: 'Library-Title',
-			description: 'Library-Description'
-			)
+		library = FactoryGirl.build(:library)
 		expect(library).to be_valid
 	end
 
 	it "is valid without a description" do
-		library = Library.new(
-			topic: "Library-title", 
-			description: nil
-			)
+		library = FactoryGirl.build(:library, description: nil)
 		expect(library).to be_valid
 	end
 	
 	it "is invalid without a topic" do
-		library = Library.new(topic: nil)
+		library = FactoryGirl.build(:library, topic: nil)
 		library.valid?
 		expect(library.errors[:topic]).to include("can't be blank")
 	end
