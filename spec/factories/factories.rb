ee225e152a46702909dcae3745a7a91209a8df2b
Veronica Moser <wicinovsky@googlemail.com>
2015-10-15 11:48:07 +0200

    Add factories and factory_girl support file

---
+++ spec/factories/factories.rb
@@ -0,0 +1,11 @@
+# This will guess the User class
+require 'factory_girl'
+
+
+FactoryGirl.define do
+  factory :user, class: User do
+    user_name "John"
+    email "john@example.com"
+    password "password"
+  end
+end
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,19 @@
+# RSpec
+# spec/support/factory_girl.rb
+
+
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+  
+end
+
