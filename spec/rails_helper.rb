77308225cbcddea3d440a906de3966d0c19a05b9
Veronica Moser <wicinovsky@googlemail.com>
2015-10-15 11:55:43 +0200

    Set up devise with rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl'
+require 'devise'
 require 'support/factory_girl.rb'
+require 'support/controller_macros.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,6 +34,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
