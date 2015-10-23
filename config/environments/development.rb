3069b351af10ae0e7eb31aada8659e71c6e32378
Veronica Moser <wicinovsky@googlemail.com>
2015-10-23 14:18:19 +0200

    Add smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+    config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :user_name            => "linkhoven@gmail.com",
+    :password             => "0_IiR0wadASbW2JyRnDF6w",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
