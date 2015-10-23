5cec6a8ebc42138eae4390adb4fd9679c2752e70
Veronica Moser <wicinovsky@googlemail.com>
2015-10-23 16:49:53 +0200

    Add confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 
 
 
-  devise :database_authenticatable, :registerable,
+  devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def role_symbols
