1a7d66454d50eb0161f7605c8e93bbfcc9643f99
Veronica Moser <wicinovsky@googlemail.com>
2015-10-30 10:10:35 +0100

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :user_name            => "linkhoven@gmail.com",
+    :password             => "0_IiR0wadASbW2JyRnDF6w",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => 'coilbee.herokuapp.com' }
 end
