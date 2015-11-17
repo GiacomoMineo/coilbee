d1a76bcf28e01f41dac3b90c598f6d0d2187b841
Veronica Moser <wicinovsky@googlemail.com>
2015-11-17 14:16:55 +0100

    Redirect after creating new library

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -35,7 +35,7 @@ class LibrariesController < ApplicationController
 
 
   		if @library_new.save 
-				redirect_to '/' 
+				redirect_to library_path(@library_new) 
   		else 
     		render 'new' 
   		end 
