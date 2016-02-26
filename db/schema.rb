7e6b908c37a983ab5aff81ac8f05b8b639409cab
Phillipp Schoppmann <schoppmp@cms.hu-berlin.de>
2016-02-26 17:28:14 +0000

    add pg_search gem and documents table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117152844) do
+ActiveRecord::Schema.define(version: 20160226163906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,16 @@ ActiveRecord::Schema.define(version: 20151117152844) do
     t.integer "library_id", null: false
     t.integer "user_id",    null: false
   end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "read_marks", force: :cascade do |t|
     t.integer  "readable_id"
