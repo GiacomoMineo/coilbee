d7f2a80ec6a29040ae7189f3d8c61a9001cb245d
Giacomo Mineo <giacomo.mineo91@gmail.com>
2016-02-29 15:26:08 +0100

    Added db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226194946) do
+ActiveRecord::Schema.define(version: 20160229134755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20160226194946) do
     t.integer  "cached_votes_score", default: 0
     t.integer  "cached_votes_up",    default: 0
     t.integer  "cached_votes_down",  default: 0
+    t.string   "author",             default: ""
+    t.integer  "readingtime",        default: 0
   end
 
   add_index "entries", ["cached_votes_down"], name: "index_entries_on_cached_votes_down", using: :btree
