3189879baeb22283b510bbbdcfc7c041e213f18c
Giacomo Mineo <giacomo.mineo91@gmail.com>
2016-02-29 18:42:16 +0100

    Added copy link to sharing options

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -4,6 +4,8 @@ function share_resource() {
 		event.preventDefault();
 
 		$share_popup = $(this).parent().parent().parent().find('.entry-share');
+		$share_popup.find('.entry-share-link').hide();
+		$share_popup.find('.share-link-alert').hide();
 		// Hide all popups
 		$('.entry-share').hide();
 		if($(this).hasClass('opened')) {
@@ -16,5 +18,19 @@ function share_resource() {
 			$(this).find('.icon-small').removeClass('glyphicon-share-alt').addClass('glyphicon-remove');
 			$share_popup.show();
 		}
+	});
+
+	// Share link
+	$('.share-link').click(function(event) {
+		event.preventDefault();
+
+		$share_link_alert = $(this).parent().find('.share-link-alert');
+		$share_link_alert.toggle();
+
+		$share_link_popup = $(this).parent().parent().parent().find('.entry-share-link');
+		$share_link_popup.toggle();
+		$share_link_popup.find('input').select();
+
+		document.execCommand('copy');
 	});
 }
