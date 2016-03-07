5444ad788f9852b083bf23fd457c859164741712
Giacomo Mineo <giacomo.mineo91@gmail.com>
2016-03-07 21:44:08 +0100

    Changed assets pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require turbolinks
-//= require bootstrap.min.js
+//= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
